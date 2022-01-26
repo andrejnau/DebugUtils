c91537ba85025a35fee12c07d0b666b028aa955f
softmob <softmob@mail.ru>
2022-01-26 20:48:01 +0300

    Do not use codecvt

--- AttachToProcessCLI.h
+++ AttachToProcessCLI.h
@@ -2,9 +2,28 @@
 #include <array>
 #include <string>
 #include <vector>
-#include <codecvt>
 
 namespace DebugUtils {
+
+std::string Utf8Encode(const std::wstring& wstr)
+{
+    if (wstr.empty())
+        return {};
+    int size_needed = WideCharToMultiByte(CP_UTF8, 0, &wstr[0], (int)wstr.size(), nullptr, 0, nullptr, nullptr);
+    std::string str(size_needed, 0);
+    WideCharToMultiByte(CP_UTF8, 0, wstr.data(), (int)wstr.size(), &str[0], size_needed, nullptr, nullptr);
+    return str;
+}
+
+std::wstring Utf8Decode(const std::string& str)
+{
+    if (str.empty())
+        return {};
+    int size_needed = MultiByteToWideChar(CP_UTF8, 0, &str[0], (int)str.size(), NULL, 0);
+    std::wstring wstr(size_needed, 0);
+    MultiByteToWideChar(CP_UTF8, 0, str.data(), (int)str.size(), &wstr[0], size_needed);
+    return wstr;
+}
 
 std::string MakeCommandLine(const std::string& app_path, const std::vector<std::string>& app_args)
 {
@@ -23,33 +42,28 @@ std::string GetDirectory(const std::string& path)
     {
         std::array<wchar_t, MAX_PATH + 1> buffer = {};
         GetCurrentDirectoryW((DWORD)buffer.size(), buffer.data());
-        std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
-        return converter.to_bytes(buffer.data());
+        return Utf8Encode(buffer.data());
     }
     return path.substr(0, loc);
 }
 
 bool AttachToProcessCLI(const std::string& app_path, uint32_t pid)
 {
-    std::string command_line = MakeCommandLine(app_path, { std::to_string(pid) });
-    std::string work_directory = GetDirectory(app_path);
-    std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
-
-    std::wstring command_line_w = converter.from_bytes(command_line);
-    std::wstring work_directory_w = converter.from_bytes(work_directory);
+    std::wstring command_line = Utf8Decode(MakeCommandLine(app_path, { std::to_string(pid) }));
+    std::wstring work_directory = Utf8Decode(GetDirectory(app_path));
 
     STARTUPINFOW startup_info = {};
     PROCESS_INFORMATION process_info = {};
 
     BOOL is_run = CreateProcessW(
         nullptr,                                       // lpApplicationName,
-        const_cast<wchar_t*>(command_line_w.c_str()),  // lpCommandLine,
+        const_cast<wchar_t*>(command_line.c_str())  ,  // lpCommandLine,
         nullptr,                                       // lpProcessAttributes,
         nullptr,                                       // lpThreadAttributes,
         FALSE,                                         // bInheritHandles,
         0,                                             // dwCreationFlags,
         nullptr,                                       // lpEnvironment,
-        work_directory_w.c_str(),                      // lpCurrentDirectory,
+        work_directory.c_str(),                        // lpCurrentDirectory,
         &startup_info,                                 // lpStartupInfo,
         &process_info                                  // lpProcessInformation
     );
